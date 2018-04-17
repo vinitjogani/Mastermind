2d240e64890fb1f2351faa4e21d1345149059f4a
Vinit Jogani <vnjogani@gmail.com>
2018-04-16 21:29:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mastermind
+
+Mastermind is a very simple JavaScript game which is a great exercise for anyone learning to code. Accompanying it, is a web app for the same game with some UI differentiations. This also serves as some boilerplate code to get a Chrome JS app up and running. Explore and tweak to your liking!
---
+++ web/main.js
@@ -0,0 +1,117 @@
+function sleep(seconds)
+{
+  var e = new Date().getTime() + (seconds * 1000);
+  while (new Date().getTime() <= e) {}
+}
+
+function str(x){
+  return x.toString()
+}
+
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function gnum(){
+
+    a= [0,1,2,3,4,5,6,7,8,9]
+    x =  randint(1,9);
+
+    b=[]
+    for (el in a)
+    {
+        if (a[el] != x)
+        { b.push(a[el]) }
+    }
+    y = b[randint(0,8)];
+    console.log(b.length);
+    c=[]
+    for (el in b)
+    {
+        if (b[el] != y)
+        { c.push(b[el]) }
+    }
+    z = c[randint(0,7)];
+
+    num = str(x) + str(y) + str(z);
+    console.log(num);
+    return num
+}
+
+function turn(number, inp){
+    exists = 0;
+    matches = 0;
+    console.log("!" + inp)
+    for (el in number){
+        if (number[el] == inp[el]){
+            exists += 1;
+            matches += 1;
+        }
+        else if (number.indexOf(inp[el]) > -1){
+            exists += 1;
+        }
+    }
+    return [exists, matches];
+}
+
+//setTimeout(game(),2000);
+match = 0;
+exist = 0;
+turns = 0;
+num = gnum()
+
+$(document).ready(function(){
+  $("body").css('background-size',($(window).width() + 50).toString() + "px " + ($(window).height() + 50).toString() + "px");
+  $("#turns").hide();
+  $("#menu ul").css("margin-top", $(document).height()/2 - $("#menu ul").height());
+
+  $('#inputnumber').keypress(function(e){
+      if(e.keyCode==13)
+      $('#submit').click();
+    });
+
+    $("#playlink").click(function(){
+      $("#menu").hide();
+      $("#screen1").hide();
+      $("#screen2").fadeIn(500);
+      $("body").css('background-image','none');
+      $("#menu a").css('color','#455ede');
+    })
+
+    $("h1").click(function(){
+      $("#screen2").hide();
+      $("#screen1").hide();
+      $("#menu").fadeIn(500);
+      $("body").css('background-image','url("bg.jpg")');
+      $("#menu a").css('color','#fff');
+    })
+
+    $("#instructionslink").click(function(){
+      $("#menu").hide();
+      $("#screen2").hide();
+      $("#screen1").fadeIn(500);
+      $("body").css('background-image','none');
+      $("#menu a").css('color','#455ede');
+    })
+
+
+$('#submit').click(function(){
+  inp = $('#inputnumber').val();
+  if(inp.length == 3){
+  $("#turns").show();
+  match = turn(num, inp)[1];
+  exist = turn(num, inp)[0];
+  turns += 1;
+  $('#turns').append('<tr><td>' + str(turns) + '</td><td>' + str(inp) + '</td><td>' + str(exist) + '</td><td>' + str(match) + '</td></tr>');
+  $('#inputnumber').val('');
+  window.scrollTo(0,document.body.scrollHeight);
+  if(match == 3){
+    alert('Victory in ' + str(turns) + ' turns!');
+    window.location.reload();
+  }
+  }
+  else{
+    alert('The number must be 3 digits long');
+  }
+});
+});
